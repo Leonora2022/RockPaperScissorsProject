820404d58dec6a1d721f9df15271fcb0809b19f5
eleanor anson <eleanoranson88@gmail.com>
2023-04-20 17:50:50 +0000

    created a playround function to display winner

--- game.js
+++ game.js
@@ -14,3 +14,24 @@ function getComputerChoice(){
     }
 }
 
+function getPlayRound(playerSelection, computerSelection){
+if((playerSelection === 'rock')&&(computerSelection === 'scissors')){
+    console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+    return 'Player won!';
+}else if((playerSelection === 'rock')&& (computerSelection === 'paper')){
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    return 'Computer won!';
+}else if((playerSelection === 'scissors')&&(computerSelection === 'paper')){
+    console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+    return 'Player won!';
+}else if((playerSelection === 'scissors')&&(computerSelection === 'rock')){
+    console.log(`You lose! ${playerSelection} beats ${computerSelection}`);
+    return 'Computer won!';
+}else if((playerSelection === 'paper')&&(computerSelection === 'rock')){
+    console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+    return 'Player won!';
+}else if((playerSelection === 'paper')&&(computerSelection === 'scissors')){
+   console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+   return 'Computer won!';
+}
+}
